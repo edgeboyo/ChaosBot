b855fa59868c7afc26320f8c51e134da2da65ce5
edgeboyo <mkolybko@gmail.com>
2020-02-25 16:22:57 +0000

    Added reverse text

--- commands/kirk.ts
+++ commands/kirk.ts
@@ -4,15 +4,21 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
 	// To get the "message" itself we join the `args` back into a string with spaces:
 	const sayMessage = args.join(" ");
-	if (
-		(sayMessage.includes("@everyone") || sayMessage.includes("@here")) &&
-		!message.member.hasPermission("ADMINISTRATOR")
-	) {
-		message.delete().catch(O_o => {});
-		return;
-	}
+	
+	const backwds = "zʎxʍʌnʎsɹbdouɯʅʞɾᴉɥƃⅎǝpɔqɐ";
 	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
 	message.delete().catch(O_o => {});
+
+	var sendMessg = "";
+	for(var i = 0; i < sayMessage.length; i++){
+		var ch = sayMessage[i];
+
+		if('a' <= ch && ch <= 'z'){
+			var no = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+			ch = backwds[backwds.length - 1 - no];
+		}
+		sendMessg = ch + sendMessg;
+	}
 	// And we get the bot to say the thing:
-	message.channel.send(sayMessage);
+	message.channel.send(sendMessg);
 }
