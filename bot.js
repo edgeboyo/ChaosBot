000629b5cbaaa947dc99a5c985f494ff361c2d57
edgeboyo <mkolybko@gmail.com>
2020-02-28 19:51:53 +0000

    New minimal build

---
+++ bot.js
@@ -0,0 +1,86 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./auth.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  let main = client.guilds.find(guild => guild.name == "Kirk_Irl");
+  let channel = main.channels.find(channel => channel.name == "general");
+  channel.send("OMG! I'm alive.");
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "chaos") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+  
+  
+  const backwds = "zʎxʍʌnʎsɹbdouɯʅʞɾᴉɥƃⅎǝpɔqɐ";
+  const backup = "Z⅄XϺɅՈꓕSꓤꝹԀONꟽ⅂ꓘᒋIH⅁ᖵƎᗡϽꓭ∀";
+  // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+  message.delete().catch(O_o => {});
+
+  var sendMessg = "";
+  for(var i = 0; i < sayMessage.length; i++){
+    var ch = sayMessage[i];
+
+    if('a' <= ch && ch <= 'z'){
+      var no = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+      ch = backwds[backwds.length - 1 - no];
+    }
+    else if ('A' <= ch && ch <= 'Z'){
+      var no = ch.charCodeAt(0) - 'A'.charCodeAt(0);
+      ch = backup[backup.length - 1 - no];
+    }
+    sendMessg = ch + sendMessg;
+  }
+  // And we get the bot to say the thing:
+  message.channel.send(sendMessg);
+  }
+});
+
+client.login(config.token);
